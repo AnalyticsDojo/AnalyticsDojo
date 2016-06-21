b86e9027db1cab124a6eab9f28344b963fa956dc
Jason Kuruzovich <jkuruzovich@gmail.com>
2016-06-21 16:16:56 -0400

    change startup

--- pm2Start.js
+++ pm2Start.js
@@ -1,81 +1,35 @@
-
 var pm2 = require('pm2');
-var nodemailer = require('nodemailer');
-var moment = require('moment-timezone');
-var _ = require('lodash');
 
-var instances = process.env.INSTANCES || 1;
-var serverName = process.env.SERVER_NAME || 'server';
-var maxMemory = process.env.MAX_MEMORY || '350M';
-var transportOptions = {
-  type: 'smtp',
-  service: 'SES',
-  auth: {
-    user: process.env.SES_USER || false,
-    pass: process.env.SES_PASSWORD
-  }
-};
-
-var mailReceiver = process.env.MAIL_RECEIVER || false;
+var instances = process.env.WEB_CONCURRENCY || -1; // Set by Heroku or -1 to scale to max cpu core -1
+var maxMemory = process.env.WEB_MEMORY || 512;    // " " "
 
 pm2.connect(function() {
-  pm2.start({
-    name: serverName,
-    script: 'server/production-start.js',
-    'exec_mode': 'cluster',
-    instances: instances,
-    'max_memory_restart': maxMemory,
-    'NODE_ENV': 'production'
-  }, function() {
-    console.log(
-      'pm2 started %s with %s instances at %s max memory',
-      serverName,
-      instances,
-      maxMemory
-    );
-    pm2.disconnect();
-  });
-});
+    pm2.start({
+        script    : 'server.js',
+        name      : 'production-app',     // ----> THESE ATTRIBUTES ARE OPTIONAL:
+        exec_mode : 'cluster',            // ----> https://github.com/Unitech/PM2/blob/master/ADVANCED_README.md#schema
+        instances : instances,
+        max_memory_restart : maxMemory + 'M',   // Auto restart if process taking more than XXmo
+        env: {                            // If needed declare some environment variables
+            "NODE_ENV": "production",
+            "AWESOME_SERVICE_API_TOKEN": "xxx"
+        },
+    }, function(err) {
+        if (err) return console.error('Error while launching applications', err.stack || err);
+        console.log('PM2 and application has been succesfully started');
 
+        // Display logs in standard output 
+        pm2.launchBus(function(err, bus) {
+            console.log('[PM2] Log streaming started');
 
-if (transportOptions.auth.user && mailReceiver) {
-  console.log('setting up mailer');
-  var transporter = nodemailer.createTransport(transportOptions);
-  var compiled = _.template(
-    'An error has occurred on server ' +
-    '<% name %>\n' +
-    'Stack Trace:\n\n\n<%= stack %>\n\n\n' +
-    'Context:\n\n<%= text %>'
-  );
+            bus.on('log:out', function(packet) {
+                console.log('[App:%s] %s', packet.process.name, packet.data);
+            });
 
-  pm2.launchBus(function(err, bus) {
-    if (err) {
-      return console.error(err);
-    }
-    console.log('event bus connected');
+            bus.on('log:err', function(packet) {
+                console.error('[App:%s][Err] %s', packet.process.name, packet.data);
+            });
+        });
 
-    bus.on('process:exception', function(data) {
-      var text;
-      var stack;
-      var name;
-      try {
-        data.date = moment(data.at || new Date())
-          .tz('America/Los_Angeles')
-          .format('MMMM Do YYYY, h:mm:ss a z');
-
-        text = JSON.stringify(data, null, 2);
-        stack = data.data.stack;
-        name = data.process.name;
-      } catch (e) {
-        return e;
-      }
-
-      transporter.sendMail({
-        to: mailReceiver,
-        from: 'team@analyticsdojo.com',
-        subject: 'Server exception',
-        text: compiled({ name: name, text: text, stack: stack })
-      });
     });
-  });
-}
+});
