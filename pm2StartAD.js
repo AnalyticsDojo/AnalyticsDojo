3e964e509196a516fdce6388c714b36e1fac86d1
Jason Kuruzovich <jkuruzovich@gmail.com>
2016-06-24 14:53:08 -0400

    Change PM2 loader

--- pm2StartAD.js
+++ pm2StartAD.js
@@ -1,6 +1,6 @@
 var pm2 = require('pm2');
 var nodemailer = require('nodemailer');
-var moment = require('moment-timezone')
+var moment = require('moment-timezone');
 var _ = require('lodash');
 
 var MACHINE_NAME = 'hk1';
@@ -17,7 +17,6 @@ var transportOptions = {
         password: process.env.SES_PASSWORD
         }
 }
-var mailReceiver = process.env.MAIL_RECEIVER || false;
 
 pm2.connect(function() {
     pm2.start({
@@ -27,8 +26,7 @@ pm2.connect(function() {
         instances : instances,
         max_memory_restart : maxMemory + 'M',   // Auto restart if process taking more than XXmo
         env: {                            // If needed declare some environment variables
-            "NODE_ENV": "production",
-            "AWESOME_SERVICE_API_TOKEN": "xxx"
+            "NODE_ENV": "production"
         },
         post_update: ["npm install"]       // Commands to execute once we do a pull from Keymetrics
     }, function() {
@@ -46,50 +44,6 @@ pm2.connect(function() {
                     console.error('[App:%s][Err] %s', packet.process.name, packet.data);
                 });
             });
-
-
         });
     });
 });
-
-if (transportOptions.auth.user && mailReceiver) {
-    console.log('setting up mailer');
-    var transporter = nodemailer.createTransport(transportOptions);
-    var compiled = _.template(
-        'An error has occurred on server ' +
-        '<% name %>\n' +
-        'Stack Trace:\n\n\n<%= stack %>\n\n\n' +
-        'Context:\n\n<%= text %>'
-    );
-
-    pm2.launchBus(function(err, bus) {
-        if (err) {
-            return console.error(err);
-        }
-        console.log('event bus connected');
-
-        bus.on('process:exception', function(data) {
-            var text;
-            var stack;
-            var name;
-            try {
-                data.date = moment(data.at || new Date())
-                    .tz('America/Los_Angeles')
-                    .format('MMMM Do YYYY, h:mm:ss a z');
-
-                text = JSON.stringify(data, null, 2);
-                stack = data.data.stack;
-                name = data.process.name;
-            } catch (e) {
-                return e;
-            }
-
-            transporter.sendMail({
-                to: mailReceiver,
-                from: 'team@analyticsdojo.com',
-                subject: 'Server exception',
-                text: compiled({ name: name, text: text, stack: stack })
-            });
-        });
-    });
-}
